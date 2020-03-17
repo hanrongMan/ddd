f0a97e0eb40aed0527393986bf7301d0a5535a62
hanrongMan <1205395133@qq.com>
2020-03-16 18:24:38 -0700

    ParkingBoyConfig 转 parkingBoy

--- src/main/java/com/tw/ddd/domain/ParkingBoyFactory.java
+++ src/main/java/com/tw/ddd/domain/ParkingBoyFactory.java
@@ -1,12 +1,18 @@
 package com.tw.ddd.domain;
 
 import com.tw.ddd.domain.model.ParkingBoyConfig;
+import com.tw.ddd.domain.strategy.FindParkingLotStrategy;
 import com.tw.ddd.domain.valueobject.ParkingBoy;
 
 
 public class ParkingBoyFactory {
 
-    public static ParkingBoy toParkingBoy(ParkingBoyConfig parkingBoyConfig) {
-        return new ParkingBoy();
+    public static ParkingBoy toParkingBoy(ParkingBoyConfig parkingBoyConfig) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        ParkingBoy parkingBoy = new ParkingBoy();
+        parkingBoy.setId(parkingBoyConfig.getId());
+        parkingBoy.setName(parkingBoyConfig.getName());
+        Class<?> clz = Class.forName(parkingBoyConfig.getFindParkingLotStrategyClassName());
+        parkingBoy.setFindParkingLotStrategy((FindParkingLotStrategy) clz.newInstance());
+        return parkingBoy ;
     }
 }
--- src/main/java/com/tw/ddd/domain/model/ParkingBoyConfig.java
+++ src/main/java/com/tw/ddd/domain/model/ParkingBoyConfig.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 public class ParkingBoyConfig {
     UUID id;
+    String name;
     private List<ParkingLot> parkingLots;
     private String findParkingLotStrategyClassName;
 
@@ -16,6 +17,14 @@ public class ParkingBoyConfig {
 
     public void setId(UUID id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public List<ParkingLot> getParkingLots() {
